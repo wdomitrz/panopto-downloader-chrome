81ce9c702446564383f53c05022a1f3bea98bd45
Yharooer <me@bentleycarr.com>
2021-06-26 20:11:36 +0100

    Add overflow menu and remove button.

--- scripts/popup.js
+++ scripts/popup.js
@@ -42,7 +42,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             const batch_dl_div = document.createElement('div');
             batch_dl_div.style.width = '100%';
             batch_dl_div.style.textAlign = 'center';
-            const batch_dl_button = createDownloadAllButton(`Download ${videoList.length} videos`);
+            const batch_dl_button = createDownloadAllButton(`Download <span id='batch_dl_counter'>${videoList.length}</span> videos`);
             batch_dl_div.appendChild(batch_dl_button);
             batch_dl_button.id = 'batch_download';
             batch_dl_button.addEventListener('click', () => {
@@ -50,10 +50,13 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             });
             document.getElementById('downloader').insertBefore(batch_dl_div, document.getElementById('downloader_list'));
         }
-        document.getElementById('downloader_list').appendChild(document.createElement('hr'));
 
         // Add a download section for each video found.
         for (let i = 0; i < videoList.length; i++) {
+            const divider = document.createElement('hr');
+            divider.id = 'divider_before_' + videoList[i].id;
+            document.getElementById('downloader_list').appendChild(divider);
+
             const div_el = document.createElement('div');
             div_el.classList.add('video_element');
             div_el.id = `video_element_${videoList[i].id}`;
@@ -71,15 +74,21 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             but.addEventListener('click', () => {
                 quickDownloadOne(videoList[i]);
             });
-            div_txcnt.append(but);
+            div_txcnt.appendChild(but);
+            const [overflowButton, overflowMenu] = createVideoMenuOverflow(videoList[i].id);
+            div_txcnt.appendChild(overflowButton);
+            div_txcnt.appendChild(overflowMenu);
             document.getElementById('downloader_list').appendChild(div_el);
-            if (i + 1 < videoList.length) {
-                document.getElementById('downloader_list').appendChild(document.createElement('hr'));
-            }
         }
         document.getElementById('loading').style.display = 'none';
         document.getElementById('downloader').style.display = 'block';
     }
+
+    // Disable remove button if only one video
+    if (videoList.length <= 1) {
+        disableRemoveOfLastVideo();
+    }
+
 });
 
 function createMaterialButton(text) {
@@ -112,6 +121,127 @@ function createDownloadAllButton(text) {
     icon.innerHTML = 'download';
     button.getElementsByClassName('mdc-button__label')[0].prepend(icon);
     return button;
+}
+
+function createMaterialIconButton(iconLabel) {
+    const button = document.createElement('button');
+    button.classList.add('mdc-icon-button');
+    button.classList.add('material-icons');
+    button.innerHTML = iconLabel;
+    mdc.ripple.MDCRipple.attachTo(button);
+    return button;
+}
+
+function createMaterialMenuListItem(label) {
+    const li = document.createElement('li');
+    li.classList.add('mdc-list-item');
+    li.setAttribute('role', 'menuitem');
+    const ripple = document.createElement('span');
+    ripple.classList.add('mdc-list-item__ripple');
+    const text = document.createElement('span');
+    text.classList.add('mdc-list-item__text');
+    text.innerHTML = label;
+    li.appendChild(ripple);
+    li.appendChild(text);
+    return li;
+}
+
+function createMaterialMenu(menuItems, anchorElement) {
+    const div = document.createElement('div');
+    div.classList.add('mdc-menu');
+    div.classList.add('mdc-menu-surface');
+    const ul = document.createElement('ul');
+    ul.classList.add('mdc-list');
+    ul.setAttribute('role', 'menu');
+    ul.setAttribute('aria-hidden', 'true');
+    ul.setAttribute('aria-orientation', 'vertical');
+    ul.setAttribute('tabindex', '-1');
+    div.appendChild(ul);
+
+    for (let i = 0; i < menuItems.length; i++) {
+        ul.appendChild(menuItems[i]);
+    }
+
+    const menu = new mdc.menu.MDCMenu(div);
+    div.openMenu = () => menu.open = true;
+    menu.setAnchorCorner(mdc.menu.Corner.BOTTOM_RIGHT);
+    if (anchorElement != null) {
+        menu.setAnchorElement(anchorElement);
+    } else {
+        console.warn('Failed to set anchor element for menu.');
+    }
+    return div;
+}
+
+function createVideoMenuOverflow(videoId) {
+    const container = document.createElement('div');
+    container.classList.add('video_overflow');
+    const button = createMaterialIconButton('more_vert');
+    container.appendChild(button);
+
+    const removeButton = createMaterialMenuListItem('Remove');
+    removeButton.classList.add('video_overflow_remove');
+    removeButton.onclick = () => removeVideoFromList(videoId);
+    // const customDlButton = createMaterialMenuListItem('Custom Download');
+    // const menu = createMaterialMenu([removeButton, customDlButton], button);
+    const menu = createMaterialMenu([removeButton], button);
+    menu.classList.add('overflow_menu');
+    button.addEventListener('click', () => menu.openMenu());
+
+    return [container, menu];
+}
+
+/**
+ * OVERFLOW MENU FUNCTIONALITY
+ */
+
+// Remove button
+function disableRemoveOfLastVideo() {
+    const removeButtons = document.getElementsByClassName('video_overflow_remove');
+    for (let i = 0; i < removeButtons.length; i++) {
+        removeButtons[i].classList.add('mdc-list-item--disabled');
+    }
+}
+
+function removeVideoFromList(videoId) {
+    // Remove from videoList.
+    videoList = videoList.filter(v => v.id !== videoId);
+
+    // Relabel download all button
+    document.getElementById('batch_dl_counter').innerHTML = videoList.length;
+
+    // Disable remove if there is only one video remaining.
+    if (videoList.length <= 1) {
+        const dlAllButton = document.getElementById('batch_download');
+        if (dlAllButton != null) {
+            dlAllButton.remove();
+        }
+        disableRemoveOfLastVideo();
+    }
+
+    // Transition out
+    const listElement = document.getElementById('video_element_' + videoId);
+    listElement.getElementsByClassName('image_container')[0].style.height = listElement.clientHeight + 'px';
+    listElement.style.height = listElement.clientHeight + 'px';
+    listElement.classList.add('transition_out');
+    listElement.style.transform = 'scaleY(1)';
+    listElement.style.opacity = '1';
+    setTimeout(() => {
+        listElement.style.height = '0px';
+        listElement.style.transform = 'scaleY(0)';
+        listElement.style.opacity = '0';
+    }, 5);
+    const divider = document.getElementById('divider_before_' + videoId);
+    divider.style.transitionProperty = 'all';
+    divider.style.transitionTimingFunction = 'ease-out';
+    divider.style.transitionDuration = '0.25s';
+    divider.style.marginTop = '0px';
+    divider.style.marginBottom = '0px';
+
+    setTimeout(() => {
+        listElement.remove();
+        divider.remove();
+    }, 250);
 }
 
 /**
